f5d1f574b244229bc80f9bf8050013457c5b9ebf
Brady Rassin <bradyr18@unc.edu>
2023-03-05 18:14:20 -0500

    finishing ex05

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,8 +1,11 @@
-"""Implementing algorithms to practice computational thinking"""
+"""Implementing algorithms to practice computational thinking."""
 __author__ = "730560378"
 
+
 def all(given_list: list[int], given_number: int) -> bool:
-    """Checks given list to see if all the numbers in the list match the given number"""
+    """Checks given list to see if all the numbers in the list match the given number."""
+    if len(given_list) == 0:
+        return False
     i: int = 0
     while i < len(given_list):
         if given_list[i] != given_number:
@@ -11,11 +14,12 @@ def all(given_list: list[int], given_number: int) -> bool:
             i += 1
     return True
 
+
 def max(input: list[int]) -> int:
-    """Return Max Number from List"""
+    """Return Max Number from List."""
     if len(input) == 0:
         raise ValueError("max() arg is an empty List")
-    maximum: int = ""
+    maximum: int = 0
     if input[0] >= input[1]:
         maximum = input[0]
     else:
@@ -27,8 +31,9 @@ def max(input: list[int]) -> int:
         idx += 1
     return maximum
 
+
 def is_equal(first_list: list[int], second_list: list[int]) -> bool:
-    """Checks both lists for deep equality"""
+    """Checks both lists for deep equality."""
     if len(first_list) != len(second_list):
         return False
     idx: int = 0
@@ -38,4 +43,3 @@ def is_equal(first_list: list[int], second_list: list[int]) -> bool:
         else:
             idx += 1
     return True
-    
---
+++ exercises/ex05_utils.py
@@ -0,0 +1,35 @@
+"""Building list utility functions continued."""
+
+__author__ = "730560378"
+
+def only_evens(numbers: list[int]) -> list[int]:
+    """Given a list, return only the even numbers."""
+    idx: int = 0
+    even_list: list[int] = []
+    while idx < len(numbers):
+        if numbers[idx] % 2 == 0:
+            even_list.append(numbers[idx])
+        idx += 1
+    return even_list
+
+def concat(list_one: list[int], list_two: list[int]) -> list[int]:
+    """Given two lists, return them as a single list."""
+    combined_list: list[int] = []
+    for element in list_one:
+        combined_list.append(element)
+    for element in list_two:
+        combined_list.append(element)
+    return combined_list
+
+def sub(a_list: list[int], start_index: int, end_index: int) -> list[int]:
+    """Given a list, a start index and an end index return the numbers."""
+    new_list: list[int] = []
+    if len(a_list) == 0 or start_index >= len(a_list) or end_index <= 0:
+        return new_list
+    if start_index < 0:
+        start_index = 0
+    if end_index > len(a_list):
+        end_index = len(a_list)
+    for idx in range(start_index, end_index):
+        new_list.append(a_list[idx])
+    return new_list
